cbe04321620cea41f4e68b3c72e3a0a84a99010b
Shaamil777 <shamilm2324@gmail.com>
2026-07-17 18:29:56 +0530

    feat:header top updated

--- src/components/global/HeaderTop.jsx
+++ src/components/global/HeaderTop.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-// import Link from 'next/link';
+import styles from './HeaderTop.module.css';
 
 export default function HeaderTop() {
     return (
         <>
         {/*  Header-Top-Section Start  */}
-        <div className="header-top-section">
-            <div className="container-fluid">
-                <div className="header-top-wrapper">
-                    <div className="header-left">
-                        <ul className="list">
-                            <li className="style-2">
+        <div className={styles.headerTopSection}>
+            <div className={styles.containerFluid}>
+                <div className={styles.headerTopWrapper}>
+                    <div className={styles.headerLeft}>
+                        <ul className={styles.list}>
+                            <li className={styles.style2}>
                                 <span>Help Line</span>
                                 <i className="fa-solid fa-phone"></i>
                                 <a href="tel:+093783575222">+91 00000 00000</a>
@@ -25,29 +25,9 @@ export default function HeaderTop() {
                             </li>
                         </ul>
                     </div>
-                    <div className="header-right">
-                        <div className="flag-wrap">
-                            <div className="flag">
-                               <i className="fa-solid fa-globe"></i>
-                            </div>
-                            <div className="nice-select" tabIndex="0">
-                                <span className="current">
-                                    English
-                                </span>
-                                <ul className="list">
-                                    <li data-value="1" className="option selected focus">
-                                        English
-                                    </li>
-                                    <li data-value="1" className="option">
-                                        Bangla
-                                    </li>
-                                    <li data-value="1" className="option">
-                                        Hindi
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="social-item">
+                    <div className={styles.headerRight}>
+                        
+                        <div className={styles.socialItem}>
                             <a href="#"><i className="fa-brands fa-linkedin"></i></a>
                             <a href="#"><i className="fa-brands fa-twitter"></i></a>
                             <a href="#"><i className="fa-brands fa-instagram"></i></a>
@@ -57,7 +37,6 @@ export default function HeaderTop() {
                 </div>
             </div>
         </div>
-
         </>
     );
 }
